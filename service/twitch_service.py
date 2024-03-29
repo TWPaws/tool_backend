ffec4e864e047f189329f229d1120a9ec8f73a0f
Andy Liu <andycom12000@gmail.com>
2024-03-30 07:58:35 +0800

    修改變數及語法讓格式維持一致

--- service/twitch_service.py
+++ service/twitch_service.py
@@ -1,11 +1,10 @@
 # ./service/twitch_service.py
 
 import requests as req
-from flask import jsonify
 
 
 class TwitchService:
-    Protocol = 'https://api.twitch.tv/helix/'
+    base_url = 'https://api.twitch.tv/helix/'
 
     def __init__(self, access_token):
         self.access_token = access_token
@@ -16,8 +15,8 @@ class TwitchService:
             "Authorization": f'Bearer {self.access_token}',
             "Client-ID": self.client_id
         }
-        Query = 'users'
-        response = req.get(self.Protocol + Query, headers=headers)
+        query = 'users'
+        response = req.get(self.base_url + query, headers=headers)
 
         if response.status_code == 200:
             user = response.json()
@@ -31,22 +30,22 @@ class TwitchService:
             "Authorization": f'Bearer {self.access_token}',
             "Client-ID": self.client_id
         }
-        Path = 'channel_points/custom_rewards?'
-        Query = f'broadcaster_id={broadcaster_id}&only_manageable_rewards=True'
+        path = 'channel_points/custom_rewards?'
+        query = f'broadcaster_id={broadcaster_id}&only_manageable_rewards=True'
         response = req.get(
-            self.Protocol + Path + Query,
+            self.base_url + path + query,
             headers=headers
         )
         if response.status_code == 200:
             while 'cursor' in response.json().get('pagination', {}):
                 cursor = response['pagination']['cursor']
-                Path = 'streams?'
-                Query = f'first=20&after={cursor}'
+                path = 'streams?'
+                query = f'first=20&after={cursor}'
                 response += req.get(
-                    self.Protocol + Path + Query,
+                    self.base_url + path + query,
                     headers=headers
                 )
-            return (response.json())
+            return response.json()
         else:
             return None
 
@@ -56,22 +55,22 @@ class TwitchService:
             "Client-ID": self.client_id
         }
 
-        Path = 'channel_points/custom_rewards/redemptions?'
-        Query = (
+        path = 'channel_points/custom_rewards/redemptions?'
+        query = (
             f'broadcaster_id={broadcaster_id}&'
             f'reward_id={reward_id}&'
             f'status=UNFULFILLED'
         )
 
         response = req.get(
-            self.Protocol + Path + Query,
+            self.base_url + path + query,
             headers=headers
         )
 
         if response.status_code == 200:
             reward_fulfill_data = response.json()
 
-            return (reward_fulfill_data)
+            return reward_fulfill_data
         else:
             return None
 
@@ -82,15 +81,15 @@ class TwitchService:
             'Content-Type': 'application/json'
         }
 
-        Query = f'channel_points/custom_rewards?broadcaster_id={broadcaster_id}'
+        query = f'channel_points/custom_rewards?broadcaster_id={broadcaster_id}'
         print(data)
         response = req.post(
-            self.Protocol + Query,
+            self.base_url + query,
             headers=headers,
             json=data
         )
         if response.status_code == 200:
-            return (response.json())
+            return response.json()
         else:
             return None
 
@@ -100,13 +99,13 @@ class TwitchService:
             'Authorization': f'Bearer {self.access_token}',
         }
 
-        Path = 'channel_points/custom_rewards?'
-        Query = f'broadcaster_id={broadcaster_id}&id={reward_id}'
+        path = 'channel_points/custom_rewards?'
+        query = f'broadcaster_id={broadcaster_id}&id={reward_id}'
 
-        response = req.delete(self.Protocol + Path + Query, headers=headers)
+        response = req.delete(self.base_url + path + query, headers=headers)
 
         if response.status_code == 204:
-            return ({'status': 'success'})
+            return {'status': 'success'}
         else:
             return None
 
@@ -117,11 +116,11 @@ class TwitchService:
             'Content-Type': 'application/json'
         }
 
-        Path = 'channel_points/custom_rewards'
-        Query = f'?broadcaster_id={broadcaster_id}&id={reward_id}'
+        path = 'channel_points/custom_rewards'
+        query = f'?broadcaster_id={broadcaster_id}&id={reward_id}'
 
         response = req.patch(
-            self.Protocol + Path + Query,
+            self.base_url + path + query,
             headers=headers,
             json=data
         )
