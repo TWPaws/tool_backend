b973314494fa689891dc81109621528cb8e8391d
Andy Liu <andycom12000@gmail.com>
2024-03-30 07:44:00 +0800

    移除user model中非必要method

--- model/user_model.py
+++ model/user_model.py
@@ -10,20 +10,5 @@ class User(UserMixin):
         self.access_token = access_token
         self.broadcaster_id = broadcaster_id
 
-    def get_username(self):
-        return self.username
-
-    def get_password(self):
-        return self.password
-
-    def get_email(self):
-        return self.email
-
-    def get_access_token(self):
-        return self.access_token
-
-    def get_broadcaster_id(self):
-        return self.broadcaster_id
-
     def get_id(self):
         return str(self.id)
