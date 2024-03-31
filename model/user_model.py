20b660c509d9784e4d5e966976e5137a848b3b96
SNWBS <75601692+SNWBS@users.noreply.github.com>
2024-03-31 19:02:34 +0800

    Class User 新增 verified 屬性

--- model/user_model.py
+++ model/user_model.py
@@ -2,7 +2,7 @@ from flask_login import UserMixin
 
 
 class User(UserMixin):
-    def __init__(self, id, nickname, username, email, password, access_token, broadcaster_id):
+    def __init__(self, id, nickname, username, email, password, access_token, broadcaster_id, verified):
         self.id = id
         self.nickname = nickname
         self.username = username
@@ -10,6 +10,7 @@ class User(UserMixin):
         self.email = email
         self.access_token = access_token
         self.broadcaster_id = broadcaster_id
+        self.verified = verified
 
     def get_id(self):
         return str(self.id)
